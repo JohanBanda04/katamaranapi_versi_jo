5e64c2f117c730d12890af6203d376d54b6fb8ce
JohanBanda04 <banda04.johan@gmail.com>
2023-05-22 15:42:50 +0800

    katamaranapi_versi_jo

---
+++ application/controllers/Agenda.php
@@ -0,0 +1,328 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Agenda extends CI_Controller
+{
+    var $table_name = 'agenda';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->get_all_rows_table($this->table_name);//GET SEMUA ROW TABEL
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function detail($id) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->get_row_detail($this->table_name, $id);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function getDetailByFolder($id_folder) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->get_row_detail_by_foreignkey($this->table_name, $id_folder, "id_folder");
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaByTanggal($tanggal) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->agendaByTanggal($tanggal);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaByRangeTanggal($tanggalAwal, $tanggalAkhir) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaByRangeTanggalIdUser($tanggalAwal, $tanggalAkhir, $idUser) //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET' || $this->uri->segment(3) == '') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->agendaByRangeTanggalIdUser($tanggalAwal, $tanggalAkhir, $idUser);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaMingguLalu() //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+
+                    $tanggalAwal = date("Y-m-d", strtotime("Monday previous week"));
+                    $tanggalAkhir = date("Y-m-d", strtotime("Sunday previous week"));
+
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaMingguIni() //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+
+                    $tanggalAwal = date("Y-m-d", strtotime("Monday this week"));
+                    $tanggalAkhir = date("Y-m-d", strtotime("Sunday this week"));
+
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaMingguDepan() //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+
+                    $tanggalAwal = date("Y-m-d", strtotime("Monday next week"));
+                    $tanggalAkhir = date("Y-m-d", strtotime("Sunday next week"));
+
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaBulanLalu() //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+
+                    $tanggalAwal = date("Y-m-d", strtotime("first day of previous month"));
+                    $tanggalAkhir = date("Y-m-d", strtotime("last day of previous month"));
+
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaBulanIni() //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+
+                    $tanggalAwal = date("Y-m-d", strtotime("first day of this month"));
+                    $tanggalAkhir = date("Y-m-d", strtotime("last day of this month"));
+
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function agendaBulanDepan() //parameter didapet dari url
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'GET') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+
+                    $tanggalAwal = date("Y-m-d", strtotime("first day of next month"));
+                    $tanggalAkhir = date("Y-m-d", strtotime("last day of next month"));
+
+                    $resp = $this->MyModel->agendaByRangeTanggal($tanggalAwal, $tanggalAkhir);
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+
+    public function create()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'POST') {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                $respStatus = $response['status'];
+                if ($response['status'] == 200) {
+                    $params = json_decode(file_get_contents('php://input'), TRUE);
+                    if (empty($params['nama'])) {//ISI NAMA PARAMETER INPUT POST NYA
+                        $respStatus = 400;
+                        $resp = array('status' => 400, 'message' => 'Input form masih salah, silahkan coba lagi');
+                    } else {
+                        $resp = $this->MyModel->insert_to_table($this->table_name, $params);
+                    }
+                    json_output($respStatus, $resp);
+                }
+            }
+        }
+    }
+
+    public function update($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'PUT' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                $respStatus = $response['status'];
+                if ($response['status'] == 200) {
+                    $params = json_decode(file_get_contents('php://input'), TRUE);
+                    $params['updated_at'] = date('Y-m-d H:i:s');
+                    if (empty($params['nama'])) { //CEK PARAMETER INPUT NYA
+                        $respStatus = 400;
+                        $resp = array('status' => 400, 'message' => 'Input form masih salah, silahkan coba lagi');
+                    } else {
+                        $resp = $this->MyModel->update_data_table($this->table_name, $id, $params);
+                    }
+                    json_output($respStatus, $resp);
+                }
+            }
+        }
+    }
+
+    public function deleteFile($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'POST' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                $respStatus = $response['status'];
+                if ($response['status'] == 200) {
+                    $params = json_decode(file_get_contents('php://input'), TRUE);
+                    $params['updated_at'] = date('Y-m-d H:i:s');
+
+                    $resp = $this->MyModel->update_data_table($this->table_name, $id, $params);
+
+                    json_output($respStatus, $resp);
+                }
+            }
+        }
+    }
+
+    public function delete($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method != 'DELETE' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE) {
+            json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+        } else {
+            $check_auth_client = $this->MyModel->check_auth_client();
+            if ($check_auth_client == true) {
+                $response = $this->MyModel->auth();
+                if ($response['status'] == 200) {
+                    $resp = $this->MyModel->delete_data_table($this->table_name, $id); //DELETE SINGLE ROW
+                    json_output($response['status'], $resp);
+                }
+            }
+        }
+    }
+}
---
+++ application/controllers/Auth.php
@@ -0,0 +1,55 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function login()
+	{
+		// json_output(200,"haha");
+		// echo 'haha';
+		// echo 'hihi';
+		$method = $_SERVER['REQUEST_METHOD'];
+		// $method telah didefinisikan pada baris diatas '$method = $_SERVER['REQUEST_METHOD'];'
+        // method berupa 'POST' juga telah ditentukan melalui kiriman request dari front end
+        // example : pada controller Web.php (katamarans/katamaran), pada baris
+        // "$response = $client->request('POST', 'auth/login', ["
+		if($method != 'POST'){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			// echo 'hoho';
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+				$params = json_decode(file_get_contents('php://input'), TRUE);
+		        $username = $params['username'];
+		        $password = $params['password'];
+		        
+				// echo $password;
+				// echo crypt('Admin123$', 'rpp');
+				//echo crypt('1234', 'rpp'); // hasilnya => rplpwJDqgNj3o
+				// echo crypt('1234', 'rplpwJDqgNj3o');
+				// echo password_hash('Admin123$', PASSWORD_DEFAULT);
+
+
+		        $response = $this->MyModel->login($username,$password);
+				json_output($response['status'],$response);
+				
+				
+			}
+		}
+	}
+
+	public function logout()
+	{	
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'POST'){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        	$response = $this->MyModel->logout();
+				json_output($response['status'],$response);
+			}
+		}
+	}
+	
+}
